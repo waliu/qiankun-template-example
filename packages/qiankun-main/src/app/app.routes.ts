a743e29354b149809a809f95b5d7508ebf57ddc0
Chen Yu <250187715@qq.com>
2024-03-08 10:54:42 +0800

    feat：添加乾坤所需要的配置

--- packages/qiankun-main/src/app/app.routes.ts
+++ packages/qiankun-main/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { EmptyRouteComponent } from "./core/empty-route.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: EmptyRouteComponent }
+];

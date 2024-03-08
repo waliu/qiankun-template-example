6377bceb05db32e3112f86072df42e63ba0c8644
Chen Yu <250187715@qq.com>
2024-03-08 10:55:34 +0800

    chore : 增加qiankun 需要的配置。

--- packages/sub-app-vue-vite/vite.config.ts
+++ packages/sub-app-vue-vite/vite.config.ts
@@ -3,13 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import qiankun from "vite-plugin-qiankun";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    qiankun('flow-graph', {
+      useDevMode: true
+    })
   ],
+
+  base: '/app-vue3-vite',
+  server: {
+    port: 3002,
+    cors: true,
+    origin: 'http://localhost:3002'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

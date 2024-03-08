7b1b8371c315c29ecfe1facd7b1da7da30345f25
Chen Yu <250187715@qq.com>
2024-03-08 11:55:00 +0800

    feat：init ng

---
+++ packages/sub-app-ng17/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sub-app-ng17-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sub-app-ng17';
+}

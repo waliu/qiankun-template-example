94ae8b94cb914a0ba8c7eb8738354561bdf78bfc
Chen Yu <250187715@qq.com>
2024-03-25 14:27:37 +0800

    init： 重置ng项目

--- packages/sub-app-ng17/src/app/app.component.ts
+++ packages/sub-app-ng17/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'sub-app-ng17-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

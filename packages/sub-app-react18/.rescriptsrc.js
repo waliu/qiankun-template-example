c70d8f67a7e9fe8010bec4652f1327a107b4a2f0
Chen Yu <250187715@qq.com>
2024-03-28 11:07:58 +0800

    chore : 修改publicpath 。让图片可以正常显示。

--- packages/sub-app-react18/.rescriptsrc.js
+++ packages/sub-app-react18/.rescriptsrc.js
@@ -7,20 +7,19 @@ module.exports = {
         config.output.libraryTarget = 'umd';
         config.output.chunkLoadingGlobal = `webpackJsonp_${name}`;
         config.output.globalObject = 'window';
+        config.output.publicPath=`http://localhost:3000/`
+        console.log(config)
         return config;
     },
 
     devServer: (_) => {
         const config = _;
-
         config.headers = {
             'Access-Control-Allow-Origin': '*',
         };
         config.historyApiFallback = true;
         config.hot = false;
         config.liveReload = false;
-        config.open = true;
-
         return config;
     },
 };
